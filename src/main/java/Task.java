459c4552c945d83be468a365926fa49fefb22c96
James <D00238925@student.dkit.ie>
2024-04-11 14:15:29 +0100

    Task class created

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,68 @@
-public class Task {
+import java.time.LocalDate;
+import java.util.Objects;
 
-    String test = "hello";
+public class Task
+    {
+        private String owner;
+        private String description;
+        private LocalDate deadline;
 
-    public static void main(String[] args) {
-        System.out.println("Hi");
+        // Constructor
+    public Task(String owner, String description, LocalDate deadline) {
+        if (deadline.compareTo(LocalDate.now()) <= 0) {
+            throw new IllegalArgumentException("Deadline must be in the future");
+        }
+        this.owner = owner;
+        this.description = description;
+        this.deadline = deadline;
     }
 
-    //comment
-}
+        public String getOwner() {
+            return owner;
+        }
+
+        public void setOwner(String owner) {
+            this.owner = owner;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public LocalDate getDeadline() {
+            return deadline;
+        }
+
+
+        public void setDeadline(LocalDate deadline) {
+            if (deadline.compareTo(LocalDate.now()) <= 0) {
+                throw new IllegalArgumentException("Deadline must be in the future");
+            }
+            this.deadline = deadline;
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Task task = (Task) o;
+            return Objects.equals(owner, task.owner) && Objects.equals(description, task.description) && Objects.equals(deadline, task.deadline);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(owner, description, deadline);
+        }
+
+        @Override
+        public String toString() {
+            return "Task{" +
+                    "owner='" + owner + '\'' +
+                    ", description='" + description + '\'' +
+                    ", deadline=" + deadline +
+                    '}';
+        }
+    }
