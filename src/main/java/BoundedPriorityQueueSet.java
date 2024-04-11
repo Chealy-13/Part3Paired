2ca3049739da688954bec9a4e7fe4dedcdc8c830
Chealy-13 <chris.healy33@gmail.com>
2024-04-11 22:31:08 +0100

    Created BoundedPriorityQueueSet class.
    Added 2 fields, a linkedList called queue to hold tasks and an int maxSize to initialise max size of queue which is 10 tasks as requested.

---
+++ src/main/java/BoundedPriorityQueueSet.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+
+public class BoundedPriorityQueueSet {
+    private final LinkedList<Task> queue;
+    private final int maxSize;
+
+    /**
+     * Creates a BoundedPriorityQueueSet with a default maximum size of 10.
+     */
+    public BoundedPriorityQueueSet() {
+        this.queue = new LinkedList<>();
+        this.maxSize = 10;
+    }
+
+    /**
+     * Creates a BoundedPriorityQueueSet with the specified maximum size.
+     * @param maxSize the maximum size of the queue
+     */
+    public BoundedPriorityQueueSet(int maxSize) {
+        this.queue = new LinkedList<>();
+        this.maxSize = maxSize;
+    }
+
+
+}
