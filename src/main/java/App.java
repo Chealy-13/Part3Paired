d8aa566e8fbe0783017794b5e57b40f13ce91752
Chealy-13 <chris.healy33@gmail.com>
2024-04-16 12:39:31 +0100

    Added App class for menu creation and added plan of what we need to do in comments.Started Main method with random generation.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,25 @@
+import java.util.Random;
+
 public class App {
 //-Random num generator for risky or safe
-//-ask user size to make queue
-//-repeat offer menu
-//-add new task
-//-view next task
-//-if queue empty after this then congrats
-//-view num of tasks remaining
-//-view space remaining in queue
-//-exit program
-//
+private static BoundedPriorityQueueSet queue;
+private static final Random random = new Random();
+
+    public static void main(String[] args) {
+        Boolean riskyOrSafe = random.nextBoolean();
+
+        //Menu switch cases here.
+
+        //-ask user size to make queue
+        //-repeat offer menu
+        //-add new task
+        //-view next task
+        //-if queue empty after this then congrats
+        //-view num of tasks remaining
+        //-view space remaining in queue
+        //-exit program
+    }
+
 //-after exit, write remaining tasks to a file
 //-ask user was it risky or safe
 //-congrats if correct console is wrong.
