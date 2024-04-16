78194f81a5d1136d50e91a78447917e1bc359b9d
Chealy-13 <chris.healy33@gmail.com>
2024-04-16 12:23:07 +0100

    Added App class for menu creation and added plan of what we need to do in comments.

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+public class App {
+//-Random num generator for risky or safe
+//-ask user size to make queue
+//-repeat offer menu
+//-add new task
+//-view next task
+//-if queue empty after this then congrats
+//-view num of tasks remaining
+//-view space remaining in queue
+//-exit program
+//
+//-after exit, write remaining tasks to a file
+//-ask user was it risky or safe
+//-congrats if correct console is wrong.
+}
